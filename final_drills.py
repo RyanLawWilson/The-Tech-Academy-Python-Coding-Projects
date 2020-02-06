29034006b75a9a71bd4779bb3961069c5eaa6fbd
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2020-02-06 13:07:35 -0700

    Create final_drills.py
    I was going to create a bunch of files python files that completed the drills in the course, but the drills were actually pretty easy.

---
+++ final_drills.py
@@ -0,0 +1,10 @@
+
+
+import random
+
+def getColor():
+    lstColors = ['Red','Yellow','Blue','Green','Purple','Pink','White','Black']
+    color = random.choice(lstColors)
+    print("The color is {}!".format(color))
+
+
